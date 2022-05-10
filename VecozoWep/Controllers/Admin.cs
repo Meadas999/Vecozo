1e1e98628e266851f138548a21eea9078c2d3456
Zein <zeinkasar123@gmail.com>
2022-05-10 15:14:53 +0200

    Alle  Views gemaakt

---
+++ VecozoWep/Controllers/Admin.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VecozoWep.Controllers
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MedewerkerOverzicht()
+        {
+            return View();
+        }
+    }
+}
---
+++ VecozoWep/Controllers/MedewerkerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VecozoWep.Controllers
+{
+    public class MedewerkerController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        public IActionResult VaardigheidToevoegen()
+        {
+            return View();
+        }
+    }
+}
